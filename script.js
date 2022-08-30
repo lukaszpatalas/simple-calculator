072f2123fe3e4c5fcea1fb809e1b1957ed9ba975
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-08-30 20:15:44 +0100

    Create function that call one of operator function

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const add = function(num1, num2) {
+const add = function (num1, num2) {
     return num1 + num2;
 };
 
-const subtract = function(num1, num2) {
+const subtract = function (num1, num2) {
     return num1 - num2;
 };
 
-const multiply = function(num1, num2) {
+const multiply = function (num1, num2) {
     return num1 * num2;
 };
 
-const divide = function(num1, num2) {
+const divide = function (num1, num2) {
     return num1 / num2;
 };
 
+const operate = function (operator, num1, num2) {
+    if (operator === "add") {
+        return add(num1, num2);
+    } else if (operator === "subtract") {
+        return subtract(num1, num2);
+    } else if (operator === "multiply") {
+        return multiply(num1, num2);
+    } else if (operator === "divide") {
+        return divide(num1, num2);
+    } else {
+        console.log("Improper operator, try again.");
+    }
+}
